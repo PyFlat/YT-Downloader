383c54783b62726bec4b5114e1a37575830bb311
PyFlat-Studios-JS <jonasfschlick@gmail.com>
2024-05-11 21:34:45 +0200

    added on_progress report

--- src/DownloaderCore/Threads/VideoDownloadThread.py
+++ src/DownloaderCore/Threads/VideoDownloadThread.py
@@ -8,13 +8,43 @@ class VideoDownloadThread(QThread):
             self.__on_finish.connect(finished_callback)
         if progress_callback != None:
             self.__on_progress.connect(progress_callback)
-
-        self.url = url
-        self.download_options = options
-    def _finish_hook(self, d):
-        pass
-    def _progress_hook(self, d):
-        pass
+        self.__is_cancled = False
+        self.__progress_counter = 0
+        self.__url = url
+        self.__download_options = options
+    def _finish_hook(self, result):
+        if result["status"] == "started":
+            self.__on_progress.emit("Postprocessing started", "")
+        else:
+            self.__on_progress.emit("Postprocessing finished", "")
+    def _progress_hook(self, result):
+        if self.__is_cancled:
+            from yt_dlp.utils import DownloadError
+            raise DownloadError("Cancelled by user")
+        
+        self.__progress_counter += 1
+        if self.__progress_counter % 20 != 0:
+            return
+        
+        match(result["status"]):
+            case "downloading":
+                downloaded_bytes = float(result["downloaded_bytes"])
+                total_bytes = 0
+                if "total_bytes" in result:
+                    total_bytes = float(result["total_bytes"])
+                elif "total_bytes_estimate" in result:
+                    total_bytes = float(result["total_bytes_estimate"])
+                else:
+                    return
+                percent_progress = round(100*downloaded_bytes/total_bytes)
+                eta = "unknown"
+                if "eta" in result and result["eta"]:
+                    eta = round(float(result["eta"]))
+                self.__on_progress.emit(f"{percent_progress}%", eta)
+            case "finished":
+                self.__on_progress.emit("","")
+            case other:
+                self.__on_progress.emit(f"Unknown status {result['status']}","")
     def run(self):
         try:
             from yt_dlp import YoutubeDL
@@ -24,7 +54,7 @@ class VideoDownloadThread(QThread):
             self.__on_finish.emit(False)
             return
         try:
-            YoutubeDL(self.download_options).download(self.url)
+            YoutubeDL(self.__download_options).download(self.__url)
         except DownloadError as download_error:
             if "urlopen error" in download_error.msg or "The read operation timed out" in download_error.msg:
                 pass#Network error
