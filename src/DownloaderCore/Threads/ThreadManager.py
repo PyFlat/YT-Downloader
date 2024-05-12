38febd2b96dd4ac838092ae58bb076d283c9fb4e
PyFlat-Studios-JS <jonasfschlick@gmail.com>
2024-05-12 19:03:24 +0200

    added threadmanager

---
+++ src/DownloaderCore/Threads/ThreadManager.py
@@ -0,0 +1,118 @@
+from PySide6.QtCore import QObject, Signal, QThread, QRunnable, QThreadPool, SignalInstance
+
+class UID():
+    __i = 0
+    def get():
+        UID.__i += 1
+        return UID.__i
+    def max():
+        return UID.__i
+    
+class ThreadWorker(QThread):
+    __on_start_callback = Signal(int)
+    __on_finish_callback = Signal(int)
+    def __init__(self, thread: QRunnable, uid: int, finish_callback: object | None = None, start_callback: object | None = None) -> None:
+        super().__init__()
+        if start_callback != None:
+            self.__on_start_callback.connect(start_callback)
+        if finish_callback != None:
+            self.__on_finish_callback.connect(finish_callback)
+        self.__thread = thread
+        self.__uid = uid
+        
+    def run(self):
+        if self.__on_start_callback != None:
+            self.__on_start_callback.emit(self.__uid)
+        self.__thread.run()
+        if self.__on_finish_callback != None:
+            self.__on_finish_callback.emit(self.__uid)
+class RunnableWrapper(QRunnable):
+    __on_start_callback = Signal(int)
+    def __init__(self, thread: QRunnable, uid: int, finish_callback: object | None = None, start_callback: object | None = None) -> None:
+        super().__init__()
+        self.__on_start_callback = start_callback
+        self.__on_finish_callback = finish_callback
+        self.__thread = thread
+        self.__uid = uid
+        self.__on_finish_callback = finish_callback
+        self.__on_start_callback = start_callback
+        
+    def run(self):
+        if self.__on_start_callback != None:
+            self.__on_start_callback(self.__uid)
+        self.__thread.run()
+        if self.__on_finish_callback != None:
+            self.__on_finish_callback(self.__uid)
+class ThreadManager():
+    def __init__(self, maxthreads: int | None = None) -> None:
+        self.__MAX_EXECUTED_TASK_TRACKING = 10000
+
+        self.__pool = QThreadPool()
+        self.__maxthreads = self.__pool.maxThreadCount()
+        if maxthreads != None:
+            self.__pool.setMaxThreadCount(maxthreads)
+            self.__maxthreads = maxthreads
+        self.__threads_pool: dict[str, QRunnable] = {}
+        self.__threads_forced: dict[str, QRunnable] = {}
+        self.__executed_tasks: list[int] = [0]*self.__MAX_EXECUTED_TASK_TRACKING
+        self.__current_tracking_index = 0
+        #Tracks the state of a task {uid: (state, forced)} forced: 0 => waiting, 1 => running, 2 => finished
+        
+    def __start(self, uid: int) -> None:
+        pass #Optional for logging
+    def __finish(self, uid: int) -> None:
+        if str(uid) in self.__threads_pool:
+            self.__threads_pool.pop(str(uid))
+        else:
+            self.__threads_forced.pop(str(uid))
+    def runTask(self, task : QRunnable, force: bool = False) -> int:
+        uid = UID.get()
+        
+        self.__executed_tasks[self.__current_tracking_index] = uid
+        self.__current_tracking_index = (self.__current_tracking_index+1)%self.__MAX_EXECUTED_TASK_TRACKING
+
+        if force:
+            worker = ThreadWorker(task, uid, self.__finish, self.__start)
+            self.__threads_forced[str(uid)] = worker
+            worker.start()
+        else:
+            worker = RunnableWrapper(task, uid, self.__finish, self.__start)
+            self.__threads_pool[str(uid)] = worker
+            self.__pool.start(worker)
+        return uid
+    def getMaxThreadCount(self) -> int:
+        return self.__maxthreads
+    def getRunningThreadCount(self, force : bool = False) -> int:
+        if force:
+            return len(self.__threads_forced)
+        return len(self.__threads_pool)
+    def queryTaskState(self, uid: int) -> str:
+        if str(uid) in self.__threads_forced:
+            return f"uid:{uid}/state:running/mode:forced"
+        if str(uid) in self.__threads_pool:
+            return f"uid:{uid}/state:unknown/mode:pool/assume:{'finished' if self.getRunningThreadCount() <= self.getMaxThreadCount() else 'queued'}"
+        if uid in self.__executed_tasks:
+            return f"uid:{uid}/state:finished/mode:unknown"
+        if uid <= UID.max():
+            return f"uid:{uid}/state:unknown/assume:finished+expired"
+        return f"uid:{uid}/state:unknown/assume:invalid"
+MANAGER = ThreadManager(10)
+
+if __name__ == "__main__":
+    import sys, random, time
+    from PySide6.QtCore import QCoreApplication
+    class randomWaiter(QRunnable):
+        def __init__(self, id) -> None:
+            super().__init__()
+            self.wt = random.randint(1,10)
+            self.id = id
+            print(f"{self.id} is created!")
+        def run(self):
+            print(f"{self.id} is started!")
+            time.sleep(self.wt)
+            print(f"{self.id} is done!")
+    __app__ = QCoreApplication()
+    for i in range (100):
+        MANAGER.runTask(randomWaiter(i), True)
+    MANAGER.getMaxThreadCount()
+    sys.exit(__app__.exec())
--- src/DownloaderCore/Threads/VideoDownloadThread.py
+++ src/DownloaderCore/Threads/VideoDownloadThread.py
@@ -1,23 +1,23 @@
-from PySide6.QtCore import QThread, Signal
-class VideoDownloadThread(QThread):
-    __on_finish = Signal(bool)
-    __on_progress = Signal(str, str)
+from PySide6.QtCore import QThread, Signal, QRunnable
+class VideoDownloadThread(QRunnable):
     def __init__(self, url: str = "https://www.youtube.com/watch?v=HBEsr0MfdmQ", options : dict[str, object] = {}, finished_callback: object| None = None, progress_callback: object | None = None) -> None:
         super().__init__()
-        if finished_callback != None:
-            self.__on_finish.connect(finished_callback)
-        if progress_callback != None:
-            self.__on_progress.connect(progress_callback)
+        self.__on_finish = finished_callback
+        self.__on_progress = progress_callback
         self.__is_cancled = False
         self.__progress_counter = 0
         self.__url = url
         self.__download_options = options
     def _finish_hook(self, result):
+        if self.__on_finish == None:
+            return
         if result["status"] == "started":
-            self.__on_progress.emit("Postprocessing started", "")
+            self.__on_finish("Postprocessing started", "")
         else:
-            self.__on_progress.emit("Postprocessing finished", "")
+            self.__on_finish("Postprocessing finished", "")
     def _progress_hook(self, result):
+        if self.__on_progress == None:
+            return
         if self.__is_cancled:
             from yt_dlp.utils import DownloadError
             raise DownloadError("Cancelled by user")
@@ -40,18 +40,19 @@ class VideoDownloadThread(QThread):
                 eta = "unknown"
                 if "eta" in result and result["eta"]:
                     eta = round(float(result["eta"]))
-                self.__on_progress.emit(f"{percent_progress}%", eta)
+                self.__on_progress(f"{percent_progress}%", eta)
             case "finished":
-                self.__on_progress.emit("","")
+                self.__on_progress("","")
             case other:
-                self.__on_progress.emit(f"Unknown status {result['status']}","")
+                self.__on_progress(f"Unknown status {result['status']}","")
     def run(self):
         try:
             from yt_dlp import YoutubeDL
             from yt_dlp.utils import DownloadError
         except ModuleNotFoundError:
             print("Youtube-dlp has not been found. Have you forgot to install it?")
-            self.__on_finish.emit(False)
+            if self.__on_finish != None:
+                self.__on_finish(False)
             return
         try:
             YoutubeDL(self.__download_options).download(self.__url)
