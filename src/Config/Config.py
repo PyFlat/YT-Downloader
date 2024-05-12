fa47bf0139866d913cdc4fb7921f6eb32765e722
Johannes Rößler <johannes.25406@gmail.com>
2024-05-12 23:07:06 +0200

    lots of new settings

--- src/Config/Config.py
+++ src/Config/Config.py
@@ -1,9 +1,18 @@
 from qfluentwidgets import *
+from PySide6.QtCore import QStandardPaths
 
 class Config(QConfig):
-    enableMinimizeAsTray = ConfigItem("Preferences", "EnableMinimizeAsTray", False, BoolValidator())
-    enableShowOnStartup = ConfigItem("Preferences", "EnableShowOnStartup", False, BoolValidator())
-    enableStartWithWindows = ConfigItem("Preferences", "EnableStartWithWindows", False, BoolValidator())
+
+    #Downloader Group
+    download_folder = ConfigItem("Downloader", "files-download-directory", QStandardPaths.writableLocation(QStandardPaths.DownloadLocation), FolderValidator())
+    ffmpeg_path = ConfigItem("Downloader", "ffmpeg-location-path", "", FolderValidator())
+    maximum_download_threads = RangeConfigItem("Downloader", "max-download-threads", 1, RangeValidator(1, 10))
+    thumbnail_streaming = ConfigItem("Downloader", "thumbnail-streaming", True, BoolValidator())
+
+    #Application Group
+
+    check_for_updates = ConfigItem("Application", "check-for-updates", True, BoolValidator())
+    log_level = OptionsConfigItem("Application", "log-level", "Info", OptionsValidator(["Info", "Debug"]))
 
 
 cfg = Config()
